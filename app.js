d706f35bd629412915a2d778a28637f77d06e78e
Kovacs Sandor <sada93@citromail.hu>
2020-12-03 10:02:11 +0100

    task14.5

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 $(document).ready(() => {
     var headerText = $("h1").text();
+    var secondHeader = $("h2").text();
     $(window).resize(() => {
       var windowWidth = $(window).width();
       var newHeader = headerText + " " + windowWidth + "px";
       $("h1").text(newHeader);
-      $(".size").text($(".box").css("width"));
-      if ($(".box").width() < 600) {
-        $(".box").css("background-color", "#9CCC65");
-      } else {
-        $(".box").css("background-color", "#FF80AB");
-      }
+      
+      var headerFontSize = $("h2").css("font-size");
+      var newSecondHeader = secondHeader + " " + headerFontSize;
+      $("h2").text(newSecondHeader);
     });
   });
   
+//-------------TASK 14.5-------------
+//-------------TASK 14.4-------------  
+    /* $(document).ready(() => {
+        var headerText = $("h1").text();
+        $(window).resize(() => {
+          var windowWidth = $(window).width();
+          var newHeader = headerText + " " + windowWidth + "px";
+          $("h1").text(newHeader);
+          $(".size").text($(".box").css("width"));
+          if ($(".box").width() < 600) {
+            $(".box").css("background-color", "#9CCC65");
+          } else {
+            $(".box").css("background-color", "#FF80AB");
+          }
+        });
+      }); */
