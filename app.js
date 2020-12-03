672bce851a25dec204d9d566da41ebf6a7a5961c
Kovacs Sandor <sada93@citromail.hu>
2020-12-03 09:25:56 +0100

    task14.4

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+    var headerText = $("h1").text();
+    $(window).resize(() => {
+      var windowWidth = $(window).width();
+      var newHeader = headerText + " " + windowWidth + "px";
+      $("h1").text(newHeader);
+      $(".size").text($(".box").css("width"));
+      if ($(".box").width() < 600) {
+        $(".box").css("background-color", "#9CCC65");
+      } else {
+        $(".box").css("background-color", "#FF80AB");
+      }
+    });
+  });
+  
